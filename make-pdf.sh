86ccd3827cae6939755752e3002856dd4ace1a32
Dominique Devriese <dominique.devriese@kuleuven.be>
2022-01-10 21:11:22 +0100

    Fix the sed script in make-pdf.sh on Linux. (#7)
    Co-authored-by: Bart Jacobs <bart.jacobs@cs.kuleuven.be>

--- make-pdf.sh
+++ make-pdf.sh
@@ -1,4 +1,5 @@
 # First, clone https://github.com/btj/jlearner and set JLEARNERPATH
+set -x -e
 cp $JLEARNERPATH/language.md $JLEARNERPATH/*.png .
 
 pandoc -o course-notes.html -f gfm \
@@ -23,19 +24,24 @@ pandoc -o course-notes.html -f gfm \
   iterators.md \
   generics.md
 pandoc --wrap=none -V documentclass=book --toc --template=latex.template --listings -o course-notes.tex course-notes.html
-sed -i '' \
-  -e 's/\chapter{Object-Oriented Programming}/\chapter{Overview}/' \
-  -e 's/\chapter{First Steps in Modular Programming (Part I)}/\part{Part I: Single-Object Abstractions}\\chapter{First Steps in Modular Programming (Part I)}/' \
-  -e 's/\chapter{Managing Complexity through Modularity and Abstraction}/\chapter[Managing Complexity: Modularity \\\& Abstraction]{Managing Complexity through Modularity and Abstraction}/' \
-  -e 's/\section{FractionLists: Representation Exposure Breaks Consistency}/\section[Representation Exposure Breaks Consistency]{FractionLists: Representation Exposure Breaks Consistency}/' \
-  -e 's/\chapter{How to properly document single-object abstractions}/\chapter[How to document single-object abstractions]{How to properly document single-object abstractions}/' \
-  -e 's/\chapter{Polymorphism}/\part{Part II: Inheritance}\\chapter{Polymorphism}/' \
-  -e 's/\chapter{Entity-relationship abstractions}/\part{Part III: Multi-Object Abstractions}\\chapter{Entity-relationship abstractions}/' \
-  -e 's/\section{Nesting class-encapsulated and package-encapsulated abstractions}/\section[Nesting class-level and package-level abstractions]{Nesting class-encapsulated and package-encapsulated abstractions}/' \
-  -e 's/\chapter{How to properly document multi-object abstractions}/\chapter[How to document multi-object abstractions]{How to properly document multi-object abstractions}/' \
-  -e 's/\chapter{Behavioral subtyping: modular reasoning about programs that use dynamic binding}/\chapter[Modular reasoning about dynamic binding]{Behavioral subtyping: modular reasoning about programs that use dynamic binding}/' \
-  -e 's/\section{Modular reasoning about programs that use dynamic binding}/\section[Modular reasoning about dynamic binding]{Modular reasoning about programs that use dynamic binding}/' \
-  -e 's/\chapter{Iterators}/\part{Part IV: Advanced Topics}\\chapter{Iterators}/' \
+if [ `uname -s` = Darwin ]; then
+  SED_IN_PLACE=(-i '')
+else
+  SED_IN_PLACE=(-i)
+fi
+sed "${SED_IN_PLACE[@]}" \
+  -e 's/\\chapter{Object-Oriented Programming}/\\chapter{Overview}/' \
+  -e 's/\\chapter{First Steps in Modular Programming (Part I)}/\\part{Part I: Single-Object Abstractions}\\chapter{First Steps in Modular Programming (Part I)}/' \
+  -e 's/\\chapter{Managing Complexity through Modularity and Abstraction}/\\chapter[Managing Complexity: Modularity \\\& Abstraction]{Managing Complexity through Modularity and Abstraction}/' \
+  -e 's/\\section{FractionLists: Representation Exposure Breaks Consistency}/\\section[Representation Exposure Breaks Consistency]{FractionLists: Representation Exposure Breaks Consistency}/' \
+  -e 's/\\chapter{How to properly document single-object abstractions}/\\chapter[How to document single-object abstractions]{How to properly document single-object abstractions}/' \
+  -e 's/\\chapter{Polymorphism}/\\part{Part II: Inheritance}\\chapter{Polymorphism}/' \
+  -e 's/\\chapter{Entity-relationship abstractions}/\\part{Part III: Multi-Object Abstractions}\\chapter{Entity-relationship abstractions}/' \
+  -e 's/\\section{Nesting class-encapsulated and package-encapsulated abstractions}/\\section[Nesting class-level and package-level abstractions]{Nesting class-encapsulated and package-encapsulated abstractions}/' \
+  -e 's/\\chapter{How to properly document multi-object abstractions}/\\chapter[How to document multi-object abstractions]{How to properly document multi-object abstractions}/' \
+  -e 's/\\chapter{Behavioral subtyping: modular reasoning about programs that use dynamic binding}/\\chapter[Modular reasoning about dynamic binding]{Behavioral subtyping: modular reasoning about programs that use dynamic binding}/' \
+  -e 's/\\section{Modular reasoning about programs that use dynamic binding}/\\section[Modular reasoning about dynamic binding]{Modular reasoning about programs that use dynamic binding}/' \
+  -e 's/\\chapter{Iterators}/\\part{Part IV: Advanced Topics}\\chapter{Iterators}/' \
   course-notes.tex
 pdflatex course-notes.tex
 pdflatex course-notes.tex
